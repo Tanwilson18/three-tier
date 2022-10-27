fb93e00f15e77430634528bf84e5fb5032a4b3bd
Tanwilson18 <totannerwilson@gmail.com>
2022-10-27 10:25:53 -0600

    First commit

--- datagrid.js
+++ datagrid.js
@@ -1,6 +1,7 @@
 // URL to your production code when you publish yoru google app script as a web app
 // You will get this URL when you publish your google apps script as a web app
-let prefix="YourWebAppURLHere" // looks like: https://script.google.com/macros/s/AKfycbz90JgtbAjrjz8c8WQGjgy8kM2GtdCAulom8XWOQyGoeX8DXM5XsmUMAcSYSSyZwwNSLq/exec
+let prefix="https://script.google.com/macros/s/AKfycbwEY-bWmgWCSnNZ8NluX_CXGczVV4wQyD_OO-WHUQ88wm2nTuu-G1inGpTlFKE2AUe0Ww/exec" 
+// looks like: https://script.google.com/macros/s/AKfycbz90JgtbAjrjz8c8WQGjgy8kM2GtdCAulom8XWOQyGoeX8DXM5XsmUMAcSYSSyZwwNSLq/exec
 const dealy_seconds = 4 // how long data validation messages are visible
 
 //example call: file:///C:/Users/Gove/three-tier/datagrid.html?employee
